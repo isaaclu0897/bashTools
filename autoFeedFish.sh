7432458d922376540f83c82a22514eb4440275c7
wei <t104306033@ntut.org.tw>
2020-04-26 14:15:40 +0800

    feat(autoFeedFish): use xdotool to feed fish automatically

---
+++ autoFeedFish.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# search window
+WID=`xdotool search --name "Google Chrome"`
+
+# activate window
+xdotool windowactivate $WID
+sleep 0.5
+
+# open happyfishbowl
+xdotool mousemove 90 40 click 1
+sleep 1
+
+# open feed
+xdotool mousemove 960 710 click 1
+sleep 1
+
+# select feed
+xdotool mousemove 900 580 click 1
+# xdotool mousemove 1025 575 click 1
+sleep 1
+
+# ===== bowl 1 =====
+xdotool mousemove 750 300 click 1
+sleep 1
+
+# go to feed place
+xdotool mousemove 800 400
+sleep 0.5
+
+# feed
+xdotool click 1
+sleep 0.5
+xdotool click 1
+sleep 5
+
+# ===== bowl 2 =====
+xdotool mousemove 715 300 click 1
+sleep 1
+
+# go to feed place
+xdotool mousemove 800 400
+sleep 0.5
+
+# feed
+xdotool click 1
+sleep 0.5
+xdotool click 1
+sleep 5
+
+# ===== bowl 3 =====
+xdotool mousemove 690 300 click 1
+sleep 1
+
+# go to feed place
+xdotool mousemove 800 400
+sleep 0.5
+
+# feed
+xdotool click 1
+sleep 0.5
+xdotool click 1
+sleep 5
+
+# end
+xdotool mousemove 1055 715 click 1
+sleep 0.5
+xdotool mousemove 1030 690 click 1
+sleep 0.5
