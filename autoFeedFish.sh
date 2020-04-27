ed2cdf979b457b6ef5c967f3def803ad7db7327f
wei <t104306033@ntut.org.tw>
2020-04-27 14:55:46 +0800

    feat(autoFeedFish): add sync param and merge command

--- autoFeedFish.sh
+++ autoFeedFish.sh
@@ -11,17 +11,17 @@ xdotool key Escape
 WID=`xdotool search --name "Google Chrome"`
 
 # activate window
-xdotool windowactivate $WID
+xdotool windowactivate --sync $WID
 
 # open happyfishbowl
-xdotool mousemove 90 40 click 1
+xdotool mousemove --sync 90 40 click 1
 
 # open feeder selection tab
-xdotool mousemove 960 710 click 1
-sleep 1
+xdotool mousemove --sync 960 710 click 1
+sleep 0.5 # wait for the tab to be opened
 
 # select feeder
-# xdotool mousemove 860 580 click 1
+xdotool mousemove 860 580 click 1
 # xdotool mousemove 900 580 click 1
 
 # xdotool mousemove 940 580 click 1
@@ -31,39 +31,29 @@ sleep 1
 # xdotool mousemove 1060 575 click 1
 
 # ===== bowl 1 =====
-xdotool mousemove 750 300 click 1
+xdotool mousemove --sync 750 300 click 1
 
-# go to feed place
-xdotool mousemove 800 400
-
-# feed
-xdotool click --repeat 2 --delay 300 1
+# feed to point
+xdotool mousemove --sync 800 400 click --repeat 2 --delay 300 1
 xdotool mousemove 300 500
 sleep 20
 
 # ===== bowl 2 =====
-xdotool mousemove 715 300 click 1
+xdotool mousemove --sync 715 300 click 1
 
-# go to feed place
-xdotool mousemove 800 400
-
-# feed
-xdotool click --repeat 2 --delay 300 1
+# feed to point
+xdotool mousemove --sync 800 400 click --repeat 2 --delay 300 1
 xdotool mousemove 300 500
 sleep 15
 
 # ===== bowl 3 =====
-xdotool mousemove 690 300 click 1
+xdotool mousemove --sync 690 300 click 1
 
-# go to feed place
-xdotool mousemove 800 400
-
-# feed
-xdotool click --repeat 2 --delay 300 1
-xdotool mousemove 300 500
+# feed to point
+xdotool mousemove --sync 800 400 click --repeat 2 --delay 300 1
 
 # end
-xdotool mousemove 1058 735 click 1
+xdotool mousemove --sync 1058 735 click 1
 
 # xdotool mousemove 1055 715 click 1
 # xdotool mousemove 1030 690 click 1
